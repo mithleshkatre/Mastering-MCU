d8cdb75b7e5118eee44a7fc44740730d1c15abd8
mithleshkatre <81501820+mithleshkatre@users.noreply.github.com>
2021-03-28 11:08:47 +0530

    Add files via upload

---
+++ STM32/stm32f4xx_drivers/Drivers/Inc/stm32f401xx_gpio_driver.h
@@ -0,0 +1,133 @@
+/*
+ * stm32f401_gpio_driver.h
+ *
+ *  Created on: Mar 13, 2021
+ *      Author: katre
+ */
+
+#ifndef INC_STM32F401XX_GPIO_DRIVER_H_
+#define INC_STM32F401XX_GPIO_DRIVER_H_
+
+#include "stm32f401xx.h"
+
+
+
+typedef struct{
+
+	uint8_t GPIO_PinNumber;    // @GPIO_PIN_PINMUMBER
+	uint8_t GPIO_PinMode;        //POSIBLE value from @GPIO_PIN_MODE
+	uint8_t GPIO_PinSpeed;       ///POSIBLE value from @GPIO_PIN_SPEED
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinAltFunMode;
+
+}GPIO_PinConfig_t;
+
+
+
+//this is the handle structure for gpio
+
+typedef struct{
+
+	GPIO_RegDef_t *pGPIOx;  //this holds the base address of gpio port
+	GPIO_PinConfig_t GPIO_PinConfig; //this holds gpio pin configuration setting
+
+}GPIO_Handle_t;
+
+
+/*****
+ * @GPIO_PIN_PINMUMBER
+ * GPIO PIN POSSIBLE MODE
+ */
+
+#define GPIO_PIN_NO_0		0
+#define GPIO_PIN_NO_1		1
+#define GPIO_PIN_NO_2		2
+#define GPIO_PIN_NO_3		3
+#define GPIO_PIN_NO_4		4
+#define GPIO_PIN_NO_5		5
+#define GPIO_PIN_NO_6		6
+#define GPIO_PIN_NO_7		7
+#define GPIO_PIN_NO_8		8
+#define GPIO_PIN_NO_9		9
+#define GPIO_PIN_NO_10		10
+#define GPIO_PIN_NO_11		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15		15
+
+
+/*****
+ * @GPIO_PIN_MODE
+ * GPIO PIN POSSIBLE MODE
+ */
+
+#define GPIO_MODE_IN		  0
+#define GPIO_MODE_OUT  		  1
+#define GPIO_MODE_ALTFN  	  2
+#define GPIO_MODE_ANALOG	  3
+
+#define GPIO_MODE_IT_FT	 	  4
+#define GPIO_MODE_IT_RT		  5
+#define GPIO_MODE_IT_RFT	  6
+
+/*****
+ * GPIO PIN POSSIBLE OUTPUT TPYE
+ */
+#define GPIO_OP_TYPE_PP		  0
+#define GPIO_OP_TYPE_OD		  1
+
+
+/*****
+ *  @GPIO_PIN_SPEED
+ * GPIO PIN POSSIBLE OUTPUT SPEED
+ */
+#define GPIO_SPEED_LOW        0
+#define GPIO_SPEED_MEDIUM     1
+#define GPIO_SPEED_FAST       2
+#define GPIO_SPEED_HIGH       3
+
+
+/*****
+ * GPIO PIN PULL UP AND PULL DOWN CONFIGURATION MACROS
+ */
+#define GPIO_NO_PUPD		0
+#define GPIO_PIN_PU			1
+#define GPIO_PIN_PD			2
+
+
+
+
+//**************************************************************************************
+// 								API supported by driver
+//****************************************************************************************
+
+//peripheral clock setup
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+
+//init and de-init
+
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle);
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
+
+// data read and write
+
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber,uint8_t Value);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+
+//IRQ configuration and ISR
+
+void GPIO_IRQConfig(uint8_t IRQNumber,uint8_t IRQPriority, uint8_t EnorDi );
+void GPIO_IRQHandling(uint8_t PinNumber);
+
+
+
+
+
+
+#endif /* INC_STM32F401XX_GPIO_DRIVER_H_ */
